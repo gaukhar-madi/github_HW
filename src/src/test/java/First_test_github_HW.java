07449b0de3c1c8869ab6012a172200adcd8aab1d
Gauhar Madi <madikgau@gmail.com>
2026-06-05 10:43:37 +0500

    Commit to main branch

---
+++ src/src/test/java/First_test_github_HW.java
@@ -0,0 +1,17 @@
+public class First_test_github_HW {
+
+        public static void main(String[] args) {
+            System.out.println("Hello GitHub!");
+            printUserInfo();
+        }
+
+        public static void printUserInfo() {
+            String name = "Gaukhar";
+            int experienceYears = 10;
+            String city = "Almaty";
+
+            System.out.println("Name: " + name);
+            System.out.println("QA Experience: " + experienceYears + " years");
+            System.out.println("City: " + city);
+        }
+    }
