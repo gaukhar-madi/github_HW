66f1e00ea2558acf096e3acb321707789cfac5bc
Gauhar Madi <madikgau@gmail.com>
2026-06-16 17:21:44 +0500

    Change city to London

--- src/src/test/java/First_test_github_HW.java
+++ src/src/test/java/First_test_github_HW.java
@@ -8,7 +8,7 @@ public class First_test_github_HW {
         public static void printUserInfo() {
             String name = "Gaukhar";
             int experienceYears = 10;
-            String city = "Astana";
+            String city = "London";
 
             System.out.println("Name: " + name);
             System.out.println("QA Experience: " + experienceYears + " years");
